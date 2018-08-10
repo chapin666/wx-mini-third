1fd558bbb491c0f81db53a4f67e2fb218fcea87b
chapin666 <997155658@qq.com>
2018-08-10 15:44:34 +0800

    update

--- test/index.js
+++ test/index.js
@@ -2,10 +2,10 @@
 
 const WechatMiniOpenAPI = require('../index');
 
-const appid = 'wxf0a1933d302b5a0e';
-const appkey = 'd3940fcc7b6ae7b2d75df989bbab1f8d';
-const aesToken = 'jhOboNh2wB7ipv9L7TSxU2LitkuPaxYh';
-const aesKey = '3l5gwSgdyO6o2t3r09futJuRYJ9tEJIpo7AWz0fGpj2';
+const appid = '';
+const appkey = '';
+const aesToken = '';
+const aesKey = '';
 
 const wechatMiniOpenAPI = new WechatMiniOpenAPI(appid, appkey, aesToken, aesKey, saveTicket, getTicket, saveToken, getToken);
 
